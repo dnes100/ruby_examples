f60f87746efd9f1db5d53a9a93a6277fb810531c
Dinesh Hyaunmikha <dinesh.hyaunmikha@gmail.com>
2018-04-09 21:48:51 +0300

    clean

--- ruby_randoms/accurate_math.rb
+++
@@ -1,9 +0,0 @@
-class AccurateMath
-  def sqrt(x)
-    guess = 1.0
-    while (x - guess*guess).abs > 0.00001
-      guess = (guess + (x/guess)) / 2.0
-    end
-    guess
-  end
-end
--- ruby_randoms/bsearch.rb
+++
@@ -1,9 +0,0 @@
-require 'benchmark'
-
-data = (0..50_00_000).to_a.shuffle
-
-
-Benchmark.bm do |x|
-  x.report(:find) { data.find { |n| n == 23_23_456 } }
-  x.report(:bsearch) { data.bsearch { |n| n == 23_23_456 } }
-end
